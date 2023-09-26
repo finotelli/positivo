70780e1695d008b8c3a97881cec68c946b71f1e4
Ana Finotelli <finotelli2004@gmail.com>
2023-09-25 22:21:05 -0300

    positivo

---
+++ program.py
@@ -0,0 +1,10 @@
+a = int(input('Digite um número: '))
+if (a < 0):
+    print('Você digitou um número negativo.') 
+if (a > 0):
+    print('Você digitou um número positivo.')
+elif (a == 0):
+    print ('O valor é igual a zero')
+    print('Você digitou um número neutro.')
+print()
+print('FIM DO PROGRAMA!')
